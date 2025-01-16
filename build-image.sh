24f3839792407920ed0c59470b966b4b61133d94
Dung Le <xdung24@gmail.com>
2025-01-16 14:30:35 +0700

    Update build script

--- build-image.sh
+++ build-image.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 VERSION=$(cat ./VERSION)
-docker buildx create --use --name mybuilder
 docker buildx build --progress=plain --platform linux/amd64 --push -t xdung24/sqld:$VERSION -t xdung24/sqld:latest .
